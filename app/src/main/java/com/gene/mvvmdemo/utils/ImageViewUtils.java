86e11b5707db566873ba6b4456b4c33ffb212629
bulgaria <18513610196@163.com>
2016-09-30 09:23:27 +0800

    ImageViewUtils

--- app/src/main/java/com/gene/mvvmdemo/utils/ImageViewUtils.java
+++ app/src/main/java/com/gene/mvvmdemo/utils/ImageViewUtils.java
@@ -7,13 +7,16 @@ import java.util.ArrayList;
  */
 public class ImageViewUtils {
     String uil1 = "http://a.hiphotos.baidu.com/image/pic/item/5d6034a85edf8db1ab6b738c0d23dd54574e7494.jpg";
-    String uil2 ="http://pic11.nipic.com/20101119/3320946_221711832717_2.jpg";
-    String uil3 ="http://www.chinagirlol.cc/data/attachment/forum/201412/03/233758hw7o7h08kkozkcwi.jpg";
-    String uil4 ="http://i2.cqnews.net/car/attachement/jpg/site82/20120817/5404a6b61e3c1197fb211d.jpg";
-    String uil5 ="http://h.hiphotos.baidu.com/image/pic/item/f9dcd100baa1cd11dd1855cebd12c8fcc2ce2db5.jpg";
-    String uil6 ="http://h.hiphotos.baidu.com/image/pic/item/c995d143ad4bd113a46490dc5eafa40f4afb05b6.jpg";
-    String uil7 ="http://f.hiphotos.baidu.com/image/pic/item/0df3d7ca7bcb0a46dfd3acd76f63f6246a60af6e.jpg";
-    String uil8 ="http://f.hiphotos.baidu.com/image/pic/item/060828381f30e924bd73bbdf48086e061c95f70c.jpg";
+    String uil2 = "http://pic11.nipic.com/20101119/3320946_221711832717_2.jpg";
+    String uil3 = "http://www.chinagirlol.cc/data/attachment/forum/201412/03/233758hw7o7h08kkozkcwi.jpg";
+    String uil4 = "http://i2.cqnews.net/car/attachement/jpg/site82/20120817/5404a6b61e3c1197fb211d.jpg";
+    String uil5 = "http://h.hiphotos.baidu.com/image/pic/item/f9dcd100baa1cd11dd1855cebd12c8fcc2ce2db5.jpg";
+    String uil6 = "http://h.hiphotos.baidu.com/image/pic/item/c995d143ad4bd113a46490dc5eafa40f4afb05b6.jpg";
+    String uil7 = "http://f.hiphotos.baidu.com/image/pic/item/0df3d7ca7bcb0a46dfd3acd76f63f6246a60af6e.jpg";
+    String uil8 = "http://f.hiphotos.baidu.com/image/pic/item/060828381f30e924bd73bbdf48086e061c95f70c.jpg";
+
+    String mImagePath[] = {uil1,uil2,uil3,uil4,uil5,uil6,uil7,uil8};
+
 
 
 
