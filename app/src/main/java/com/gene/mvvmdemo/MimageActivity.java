a987bf378ce4e5a8c93b546989c2b7fe60123a30
bulgaria <18513610196@163.com>
2016-10-08 11:35:13 +0800

    error

--- app/src/main/java/com/gene/mvvmdemo/MimageActivity.java
+++ app/src/main/java/com/gene/mvvmdemo/MimageActivity.java
@@ -1,14 +1,40 @@
 package com.gene.mvvmdemo;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.databinding.BindingAdapter;
+import android.databinding.DataBindingUtil;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.gene.mvvmdemo.databinding.ActivityMimageBinding;
+import com.gene.mvvmdemo.utils.ImageViewUtils;
+import com.squareup.picasso.Picasso;
 
 public class MimageActivity extends Activity {
+
+    private ActivityMimageBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mimage);
+        binding = DataBindingUtil.setContentView(
+                this,R.layout.activity_mimage);
+        binding.setActivity(this);
+        binding.setImageUrl(ImageViewUtils.nextImgUrl());
+    }
+
+    public void changeImage(View view){
+        Toast.makeText(MimageActivity.this, "papapapapa", Toast.LENGTH_SHORT).show();
+        binding.setImageUrl(ImageViewUtils.nextImgUrl());
+    }
+
+    @BindingAdapter({"imageUrl", "error"})
+    public static void loadImage(ImageView view, String url, Drawable error) {
+        Picasso.with(view.getContext()).load(url).error(error).into(view);
     }
 }
--- app/src/main/java/com/gene/mvvmdemo/utils/ImageViewUtils.java
+++ app/src/main/java/com/gene/mvvmdemo/utils/ImageViewUtils.java
@@ -1,23 +1,25 @@
 package com.gene.mvvmdemo.utils;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Administrator on 2016/9/29.
  */
 public class ImageViewUtils {
-    String uil1 = "http://a.hiphotos.baidu.com/image/pic/item/5d6034a85edf8db1ab6b738c0d23dd54574e7494.jpg";
-    String uil2 = "http://pic11.nipic.com/20101119/3320946_221711832717_2.jpg";
-    String uil3 = "http://www.chinagirlol.cc/data/attachment/forum/201412/03/233758hw7o7h08kkozkcwi.jpg";
-    String uil4 = "http://i2.cqnews.net/car/attachement/jpg/site82/20120817/5404a6b61e3c1197fb211d.jpg";
-    String uil5 = "http://h.hiphotos.baidu.com/image/pic/item/f9dcd100baa1cd11dd1855cebd12c8fcc2ce2db5.jpg";
-    String uil6 = "http://h.hiphotos.baidu.com/image/pic/item/c995d143ad4bd113a46490dc5eafa40f4afb05b6.jpg";
-    String uil7 = "http://f.hiphotos.baidu.com/image/pic/item/0df3d7ca7bcb0a46dfd3acd76f63f6246a60af6e.jpg";
-    String uil8 = "http://f.hiphotos.baidu.com/image/pic/item/060828381f30e924bd73bbdf48086e061c95f70c.jpg";
+    private static final Random sRandom = new Random();
 
-    String mImagePath[] = {uil1,uil2,uil3,uil4,uil5,uil6,uil7,uil8};
+    private static final String[] IMG_URLS= new String[]{
+            "http://a.hiphotos.baidu.com/image/pic/item/5d6034a85edf8db1ab6b738c0d23dd54574e7494.jpg"
+            ,"http://pic11.nipic.com/20101119/3320946_221711832717_2.jpg"
+            ,"http://www.chinagirlol.cc/data/attachment/forum/201412/03/233758hw7o7h08kkozkcwi.jpg"
+            ,"http://i2.cqnews.net/car/attachement/jpg/site82/20120817/5404a6b61e3c1197fb211d.jpg"
+            ,"http://h.hiphotos.baidu.com/image/pic/item/f9dcd100baa1cd11dd1855cebd12c8fcc2ce2db5.jpg"
+            ,"http://h.hiphotos.baidu.com/image/pic/item/c995d143ad4bd113a46490dc5eafa40f4afb05b6.jpg"
+            ,"http://f.hiphotos.baidu.com/image/pic/item/0df3d7ca7bcb0a46dfd3acd76f63f6246a60af6e.jpg"
+            ,"http://f.hiphotos.baidu.com/image/pic/item/060828381f30e924bd73bbdf48086e061c95f70c.jpg"};
 
-
-
-
+    public static String nextImgUrl() {
+        return IMG_URLS[sRandom.nextInt(IMG_URLS.length)];
+    }
 }
