b611d87f2d2b056ecd38540340cc09c8337cf64c
Li Wang <xiaoBulgaria@163.com>
2017-02-07 17:17:35 +0800

    Update MyClass.java

--- test/src/main/java/com/example/MyClass.java
+++ test/src/main/java/com/example/MyClass.java
@@ -1,12 +1,26 @@
 package com.example;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import javax.smartcardio.Card;
+
 public class MyClass {
+    enum Suit {CLUB,DIAMOND,HEART,SPADE}
+    enum Rank {ACE ,DEUCE,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE,TEN,JACY,QUEEN,KING}
+
+
+    static Collection<Suit> suits = Arrays.asList(Suit.values());
+    static Collection<Rank> ranks = Arrays.asList(Rank.values());
 
     public static void main(String[] args) {
-        System.out.print("hello word");
+        for(Suit suit : suits){
+            for(Rank rank : ranks){
+                System.out.print(suit + "---" + rank + "    ");
+            }
+        }
     }
-
-
-
-
 }
