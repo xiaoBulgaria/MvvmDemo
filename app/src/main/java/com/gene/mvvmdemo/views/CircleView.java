64726b06082bef8a387e965c584c5eb2de61e50b
wangli <xiaobulgaria@163.com>
2017-11-09 16:17:09 +0800

    test

---
+++ app/src/main/java/com/gene/mvvmdemo/views/CircleView.java
@@ -0,0 +1,40 @@
+package com.gene.mvvmdemo.views;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Android on 2017/11/9.
+ */
+
+public class CircleView extends View{
+
+
+    Paint paint = new Paint();
+
+    public CircleView(Context context) {
+        super(context);
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawCircle(300,300,200,paint);
+    }
+}
